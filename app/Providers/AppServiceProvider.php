be9e53fc59faf50a145e3924d0a26ce8230a8f80
Fraidy <fraidygonzalezreinaldo@gmail.com>
2026-01-19 21:21:32 -0500

    feat: Changed verbs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        JsonResource::withoutWrapping();
+
+        Route::resourceVerbs([
+            'create' => 'crear',
+            'show' => 'mostrar',
+            'edit' => 'editar',
+        ]);
     }
 }

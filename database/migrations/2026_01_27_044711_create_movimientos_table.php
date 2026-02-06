6309eb32cb1e9b5c386985f04f14b889bf951b03
Fraidy <fraidygonzalezreinaldo@gmail.com>
2026-02-05 20:55:45 -0500

    fix: Add default values to the fields

--- database/migrations/2026_01_27_044711_create_movimientos_table.php
+++ database/migrations/2026_01_27_044711_create_movimientos_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('movimientos', function (Blueprint $table) {
             $table->id();
-            $table->integer('entradas');
-            $table->integer('salidas');
-            $table->integer('traslados');
-            $table->integer('venta_diaria');
+            $table->integer('entradas')->default(0);
+            $table->integer('salidas')->default(0);
+            $table->integer('traslados')->default(0);
+            $table->integer('venta_diaria')->default(0);
             $table->timestamps();
 
             $table->foreignId('producto_id')->constrained('productos')->cascadeOnDelete();
-            $table->foreignId('destino_id')->constrained('destinos')->cascadeOnDelete();
+            $table->foreignId('destino_id')->nullable()->constrained('destinos')->cascadeOnDelete();
             $table->foreignId('usuario_id')->constrained('users')->cascadeOnDelete();
             $table->foreignId('inventario_tienda_id')->constrained('inventario_tiendas')->cascadeOnDelete();
         });
